62a01e395616b5330064b036297aea5a603f707b
Marty T <120425148+tippmar-nr@users.noreply.github.com>
2024-05-23 16:04:43 -0500

    chore: Cleanup code samples (#23)

--- elastic-beanstalk/ElasticBeanstalkLinux/Pages/Index.cshtml.cs
+++ elastic-beanstalk/ElasticBeanstalkLinux/Pages/Index.cshtml.cs
@@ -1,7 +1,6 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ElasticBeanstalkTestLinux.Pages;
+namespace ElasticBeanstalkLinux.Pages;
 
 public class IndexModel : PageModel
 {
